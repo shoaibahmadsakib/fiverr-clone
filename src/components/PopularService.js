91c06ae6019fc4a7d939768dedc4212a97cace1f
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2021-09-11 15:47:54 +0600

    aa

--- src/components/PopularService.js
+++ src/components/PopularService.js
@@ -3,48 +3,42 @@ import "../css/PopularService.css";
 import Carousel from "react-elastic-carousel";
 import { Container } from "@material-ui/core";
 
-
-
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 550, itemsToShow: 2, itemsToScroll: 2 },
   { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4}
+  { width: 1200, itemsToShow: 4 },
 ];
 const PopularService = () => {
   return (
     <Container className="ComponentSpace">
-      <Carousel itemsToScroll={2}  breakPoints={breakPoints}
- outerSpacing={0}
-      >
-        <div className="image_1">
-          <h3>service</h3>
-          <p>thia ia populae</p>
+      <Carousel itemsToScroll={2} breakPoints={breakPoints} outerSpacing={0}>
+        <div className="image_1 text_styleImg">
+          <p>Logo design</p>
+          <h3>brand</h3>
         </div>
-        <div className="image_1">
-   
-          <h3>service</h3>
+        <div className="image_2 text_styleImg">
           <p>thia ia populae</p>
+          <h3>Wordpress</h3>
         </div>
-        <div className="image_1">
-     
-          <h3>service</h3>
+        <div className="image_3 text_styleImg">
           <p>thia ia populae</p>
+          <h3>Web design</h3>
         </div>
-        <div className="image_1">
+        <div className="image_4 text_styleImg">
           {" "}
-          <h3>service</h3>
           <p>thia ia populae</p>
+          <h3>service</h3>
         </div>
-        <div className="image_1">
+        <div className="image_5 text_styleImg">
           {" "}
-          <h3>service</h3>
           <p>thia ia populae</p>
+          <h3>service</h3>
         </div>
-        <div className="image_1">
+        <div className="image_6 text_styleImg">
           {" "}
-          <h3>service</h3>
           <p>thia ia populae</p>
+          <h3>service</h3>
         </div>
       </Carousel>
     </Container>
